72e3b0640d1b2a5736ca3fae51667cd03a7add5e
thiago passeri <119812332+thiagopasseri@users.noreply.github.com>
2024-11-16 19:47:08 +0100

    add UI

--- script.js
+++ script.js
@@ -5,52 +5,93 @@ console.log("Hello World")
 const choices = ["rock","paper","scissors"]; // [0-rock,1-paper,2-scissors]
 
 //initialize table of results
-const resultsTable = [["tie","computer","human"], ["human", "tie", "computer"], ["computer","human", "tie"]];
+const resultsTable = [["tie","computer","human"],
+                      ["human", "tie", "computer"], 
+                      ["computer","human", "tie"]];
 
 //initialize human and computer scores
 let humanScore = 0;
 let computerScore = 0;
 
+//initialize documents variables
+const buttonContainerElement = document.querySelector("#btn-container");
+const playerChoiceElement = document.querySelector("#human-choice-text");
+const computerChoiceElement = document.querySelector("#computer-choice-text");
+const resultElement = document.querySelector("#result-text");
+const scoreElement = document.querySelector("#score")
+
+const rockBtn = document.querySelector("#rock-btn");
+const paperBtn = document.querySelector("#paper-btn");
+const scissorsBtn = document.querySelector("#scissors-btn");
+const goBtn = document.querySelector("#go-btn")
+let humanChoice = '';
+
+playerChoiceElement.textContent = "TESTE";
+
+//function that changes the text with the choice selected
+function displayChoiceText(choice){
+    playerChoiceElement.textContent = choice;
+}
+
+
+//manages the click in the button container
+function manageClick(event) {
+    let target = event.target;
+    if (target.tagName ==='BUTTON') {
+        switch(target.value) {
+            case "rock":
+                humanChoice = "rock";
+                break;
+            case "paper":
+                humanChoice = "paper";
+                break;
+            case "scissors":
+                humanChoice = "scissors";
+                break;
+        }
+        playerChoiceElement.textContent = humanChoice;
+    }
+}
+
+
+buttonContainerElement.addEventListener("click", (e) => manageClick(e));
+goBtn.addEventListener("click", () => {
+        if(humanChoice){
+            playRound();
+            humanChoice= '';
+            playerChoiceElement.textContent='';
+        }
+})
+
+
 //function that returns computer choices (rock, paper or scissors)
 function getComputerChoice() {
     let choiceNum = parseInt(Math.random()*3);
-    // return choices[choiceNum];
+    // return choices[choiceNum] - rock, paper or scissors
     return choiceNum;
 }
 
-//function that prompts the user and returns its choice (rock, paper, scissors)
-function getHumanChoice() {
-    let humanInput = prompt("Rock, Paper or Scissors?");
-    if (!humanInput) return getHumanChoice();
-    let humanChoice = humanInput.toLowerCase();
-    switch(humanChoice){
-        case "rock": return 0;
-        case "paper": return 1;
-        case "scissors": return 2;
-        default: return getHumanChoice();
-    }
-}
+
 
 //function that plays a round: prompts the user, updates score and and logs winner
 function playRound() { 
     //initialize the choices for the round
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
+    let computerChoiceNumber = getComputerChoice();
+    let humanChoiceNumber = choices.indexOf(humanChoice);
 
-    let result = resultsTable[humanChoice][computerChoice];
+    let result = resultsTable[humanChoiceNumber][computerChoiceNumber];
+    console.log(result);
     if (result == "tie") {
-        // console.log("empate!");
-        alert(`TIE\n${choices[humanChoice]} e ${choices[computerChoice]} empatam!\nvocê: ${humanScore} \ncomp: ${computerScore}`);
-
+        resultElement.textContent = "TIE";
     }
     else if (result =="human"){
-        // console.log("você venceu!");
         humanScore++;
         if (humanScore == 5) {
             alert("você ganhou o JOGO!");
+            resetGame();
             return;
         }
-        alert(`você ganhou! ${choices[humanChoice]} ganha de ${choices[computerChoice]} \nvocê: ${humanScore} \ncomp: ${computerScore}`);
+        resultElement.textContent = "GANHOU";
 
     } 
     else if (result =="computer") {
@@ -58,23 +99,53 @@ function playRound() {
         computerScore++;
         if (computerScore == 5) {
             alert("você perdeu o JOGO!");
+            resetGame();
             return;
         }
-        alert(`você perdeu! ${choices[computerChoice]} ganha de ${choices[humanChoice]} \nvocê: ${humanScore} \ncomp: ${computerScore}`);
-
+        resultElement.textContent = "PERDEU";
     }
-    // console.log(`você: ${humanScore} `);
-    // console.log(`comp: ${computerScore}`);
+    scoreElement.textContent = `you: ${humanScore}      computer: ${computerScore}`;
 }
-//game loop
-while(humanScore < 5 && computerScore < 5)
-{
-    playRound();
-}
-console.log(getHumanChoice());
 
-// for (let i=0; i<10; i++)
-// {
-//     console.log(getComputerChoice());
+function resetGame (){
+    humanScore = 0;
+    computerScore = 0;
+    humanChoice ='';
+    scoreElement.textContent = `you: ${humanScore}      computer: ${computerScore}`;
+    resultElement.textContent = '';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function that gets the player choice
+
+// function getPlayerChoice (event){
+//     const selectedButton = event.target;
+//     const choice = selectedButton.value;
+//     return choice;
 // }
-
+//function that prompts the user and returns its choice (rock, paper, scissors)
+// function getHumanChoice() {
+//     let humanInput = prompt("Rock, Paper or Scissors?");
+//     if (!humanInput) return getHumanChoice();
+//     let humanChoice = humanInput.toLowerCase();
+//     switch(humanChoice){
+//         case "rock": return 0;
+//         case "paper": return 1;
+//         case "scissors": return 2;
+//         default: return getHumanChoice();
+//     }
+// }
